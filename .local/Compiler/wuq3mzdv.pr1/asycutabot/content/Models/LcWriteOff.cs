95dd184870a3c6592d8398bcec64d3e741b36d38
Khaledul Islam <khaledul.islam@hotmail.com>
2026-09-08 12:32:27 +0600

    Initial commit

---
+++ .local/Compiler/wuq3mzdv.pr1/asycutabot/content/Models/LcWriteOff.cs
@@ -0,0 +1,12 @@
+
+namespace asycutabot.Models
+{
+    public class LcWriteOff
+    {
+        public string OfficeCode { get; set; }
+        public string RegistrationYear { get; set; }
+        public string RegistrationSerial { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string InvoiceValue { get; set; }
+    }
+}
